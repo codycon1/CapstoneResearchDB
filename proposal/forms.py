3e41205c6fda2548ac45fc0194a816c00e10bd3c
Cody <codycon1@gmail.com>
2022-03-27 13:10:17 -0600

    Proposal: [CR]UD

---
+++ proposal/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import proposal
+
+
+class SubmitProposalForm(forms.ModelForm):
+    class Meta:
+        model = proposal
+        fields = [
+            'title',
+            'description',
+        ]
+    def __str__(self):
+        return self.title
+
+    # Default approval and user to false / current login user
---
+++ proposal/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from proposal import views
+
+urlpatterns = [
+    path('submit', views.submit_proposal),
+    path('mysubmissions', views.my_submissions),
+]
