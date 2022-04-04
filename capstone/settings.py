34aeb986a23b91f6c2e3a850236c651b0943e208
Ejwhite25 <evanjwhite2599@gmail.com>
2022-04-04 12:40:18 -0600

    Gor default ahutor field working

--- capstone/settings.py
+++ capstone/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+current_user_Name = ''
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
--- home/views.py
+++ home/views.py
@@ -18,9 +18,10 @@ def home(request):
 
 
 def viewSubmissions(request):
-    context = {'accepted': Project.objects.filter(approval=True), 'pending': Project.objects.filter(approval=False)}
-    context = initialize_context(request)
-    user = context['user']
+    userInfo = initialize_context(request)
+    context = {'accepted': Project.objects.filter(approval=True, projectAuthor=userInfo['user']['name']),
+               'pending': Project.objects.filter(approval=False, projectAuthor=userInfo['user']['name']),
+               'user': userInfo['user']}
     return render(request, 'myproposals.html', context)
 
 
@@ -37,30 +38,28 @@ def SearchRequest(request):
 
 def saveFileUpload(request):
     context = initialize_context(request)
-    user = context['user']
-    form = UploadFileForm()
+    authorName = context['user']['name']
+    form = UploadFileForm(initial={'projectAuthor':authorName})
     try:
         if request.method == 'POST':
+            print(authorName)
             form = UploadFileForm(request.POST, request.FILES)
             if form.is_valid():
                 form.save()
                 project_info = form.instance()
-                # context['form'] = form
-                # context['project_info'] = project_info
-                return render(request, 'fileupload.html', {'form': form, 'project_info': project_info, 'user': user})
+                return render(request, 'fileupload.html',
+                              {'form': form, 'project_info': project_info, 'user': context['user']})
             else:
                 form = UploadFileForm()
-                # context['form'] = form
-            return render(request, 'fileupload.html', {'form': form, 'user': user})
+            return render(request, 'fileupload.html', {'form': form, 'user': context['user']})
     except Exception as identifier:
         print(identifier)
-    return render(request, 'fileupload.html', {'form': form, 'user': user})
+    return render(request, 'fileupload.html', {'form': form, 'user': context['user']})
 
 
 def approveProposal(request):
-    context = {'pending': Project.objects.filter(approval=False)}
-    contextUser = initialize_context(request)
-    context['user'] = contextUser
+    userInfo = initialize_context(request)
+    context = {'pending': Project.objects.filter(approval=False), 'user': userInfo['user']}
     if request.method == 'POST':
         id = request.POST.get('id', 0)
         proposalObject = Project.objects.get(id=request.POST['id'])
