710d53937d679a928c7a557090b46b1b93c36662
Ejwhite25 <evanjwhite2599@gmail.com>
2022-04-25 11:44:20 -0600

    File upload for results and dataset working

--- home/models.py
+++ home/models.py
@@ -10,7 +10,8 @@ class Project(models.Model):
     projectTitle = models.CharField(max_length=100, verbose_name="Project Title")
     projectDescription = models.CharField(max_length=256, verbose_name="Project Description")
     proposalFile = models.FileField(
-        validators=[FileExtensionValidator(allowed_extensions=['pdf', 'doc', 'csv', 'xlsx', 'docx', 'txt'])], # TODO: Remove .txt
+        validators=[FileExtensionValidator(allowed_extensions=['pdf', 'doc', 'csv', 'xlsx', 'docx', 'txt'])],
+        # TODO: Remove .txt
         upload_to='projects/proposals', verbose_name="Proposal")
     approval = models.BooleanField(default=False)
 
@@ -23,9 +24,27 @@ class Project(models.Model):
 class ProjectFile(models.Model):
     projectID = models.ForeignKey(Project, on_delete=models.CASCADE)
     userEmail = models.EmailField(max_length=256)
-    file = models.FileField(upload_to='projects/datasets')
+    file = models.FileField(
+        validators=[FileExtensionValidator(allowed_extensions=['pdf', 'doc', 'csv', 'xlsx', 'docx', 'txt'])],
+        # TODO: Remove .txt
+        upload_to='projects/Datasets', verbose_name="Results")
     # 0 = proposal, 1 = data, 2 = results
-    type = models.IntegerField(blank=True, default=0)
+    type = models.IntegerField(blank=True, default=1)
+    uploadDate = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class ResultFile(models.Model):
+    projectID = models.ForeignKey(Project, on_delete=models.CASCADE)
+    userEmail = models.EmailField(max_length=256)
+    resultFile = models.FileField(
+        validators=[FileExtensionValidator(allowed_extensions=['pdf', 'doc', 'csv', 'xlsx', 'docx', 'txt'])],
+        # TODO: Remove .txt
+        upload_to='projects/results', verbose_name="Results")
+    # 0 = proposal, 1 = data, 2 = results
+    type = models.IntegerField(blank=True, default=2)
     uploadDate = models.DateTimeField(auto_now=True)
 
     def __str__(self):
--- home/views.py
+++ home/views.py
@@ -35,26 +35,33 @@ def home(request):
 
 def viewSubmissions(request):
     userInfo = request.session
-
-    #TODO: Process additional file upload here
     if request.method == 'POST':
         file = request.POST.get('file', None)
         filetype = request.POST.get('type', None)
         id = request.POST.get('id', None)
         file = request.FILES['file']
+        filetype = int(filetype)
         projectTitle = request.POST.get('dataTitle')
-        if file is not None and filetype is not None:
-            projectfile_instance = ProjectFile.objects.update_or_create(projectID=Project(id=id,projectTitle=projectTitle,email=request.session['user']['email']))
+        if file is not None and filetype == 1:
+            projectfile_instance = ProjectFile.objects.create(
+                projectID=Project(id=id, projectTitle=projectTitle, email=request.session['user']['email']))
             projectfile_instance.userEmail = request.session['user']['email']
             print(request.POST.get('id', None))
             projectfile_instance.file = file
             projectfile_instance.type = request.POST.get('type', None)
             projectfile_instance.save()
+        if file is not None and filetype == 2:
+            resultFile_instance = ResultFile.objects.create(
+                projectID=Project(id=id, projectTitle=projectTitle, email=request.session['user']['email']))
+            resultFile_instance.userEmail = request.session['user']['email']
+            print(request.POST.get('id', None))
+            resultFile_instance.resultFile = file
+            resultFile_instance.type = request.POST.get('type', None)
+            resultFile_instance.save()
     context = {'accepted': Project.objects.filter(approval=True, email=userInfo['user']['email']),
                'pending': Project.objects.filter(approval=False, email=userInfo['user']['email']),
                'user': userInfo['user']}
     return render(request, 'myproposals.html', context)
-
 
 
 def SearchRequest(request):
